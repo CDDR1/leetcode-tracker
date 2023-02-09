be6a98be90a8bec4b2678a208a65e81118ee11b2
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-09 11:22:11 -0500

    Updating question crontroller to use the question service methods.

--- server/src/controllers/questionController.js
+++ server/src/controllers/questionController.js
@@ -1,20 +1,27 @@
+const questionService = require("../services/questionService");
+
 const getAllQuestions = (req, res) => {
+  const allQuestions = questionService.getAllQuestions();
   res.send("Get all questions");
 };
 
 const getOneQuestion = (req, res) => {
+  const question = questionService.getOneQuestion();
   res.send("Get single question by id");
 };
 
 const createNewQuestion = (req, res) => {
+  const createdQuestion = questionService.createNewQuestion();
   res.send("Add a new question");
 };
 
 const updateOneQuestion = (req, res) => {
+  const updatedQuestion = questionService.updateOneQuestion();
   res.send("Update existing question by id");
 };
 
 const deleteOneQuestion = (req, res) => {
+  questionService.deleteOneQuestion();
   res.send("Delete existing question by id");
 };
 
