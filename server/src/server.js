ba3d22a3861de273014a1e4da74f5a85e5ecfcfe
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-09 11:43:37 -0500

    Installed and cofigured body-parser.

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 // API version 1
 const v1QuestionRouter = require("./v1/routes/questionRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use("/api/v1/questions", v1QuestionRouter);
 
 app.listen(PORT, () => {
