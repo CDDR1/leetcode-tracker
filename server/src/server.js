fbfe20358cc21e2821bbfe765a103d3525b37f1d
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-09 12:32:26 -0500

    Setting up db connection with Mongoose.

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const bodyParser = require("body-parser");
 
@@ -9,9 +10,20 @@ const v1QuestionRouter = require("./v1/routes/questionRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(bodyParser.json());
+
+// Routes
 app.use("/api/v1/questions", v1QuestionRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT} 🚀🚀🚀`);
-});
+// Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT} 🚀🚀🚀`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
