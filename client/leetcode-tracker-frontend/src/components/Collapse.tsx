f1b90b0fc2407706f7d142616cae820eca6156e7
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-02-15 10:34:48 -0500

    Adding data fetching

--- client/leetcode-tracker-frontend/src/components/Collapse.tsx
+++ client/leetcode-tracker-frontend/src/components/Collapse.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+type CollapseProps = {
+  questionName: string;
+  questionNumber: number;
+  questionDifficulty: string;
+};
 
-const Collapse = () => {
+const Collapse = ({ questionName, questionNumber, questionDifficulty }: CollapseProps) => {
   return (
     <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-      <div className="collapse-title text-lg font-medium"><h3>Two Sum</h3></div>
+      <div className="collapse-title text-lg font-medium">
+        <h3>{questionName}</h3>
+      </div>
       <div className="collapse-content">
         <div className="flex flex-col gap-1">
-          <span>Question number: {1}</span>
-          <span>Difficulty: {"Easy"}</span>
+          <span>Question number: {questionNumber}</span>
+          <span>Difficulty: {questionDifficulty}</span>
         </div>
       </div>
     </div>
--- client/leetcode-tracker-frontend/src/components/QuestionList.tsx
+++ client/leetcode-tracker-frontend/src/components/QuestionList.tsx
@@ -1,21 +1,28 @@
 import Collapse from "./Collapse";
 import { useState, useEffect } from "react";
 
+type QuestionsState = {
+  _id: string;
+  number: number;
+  title: string;
+  difficulty: string;
+};
+
 const QuestionList = () => {
-  const [questions, setQustions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
 
   useEffect(() => {
     fetch("https://leetcode-tracker-production.up.railway.app/api/v1/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => setQuestions(data))
       .catch((error) => console.log(error));
   }, []);
 
   return (
     <div className="mx-auto w-2/3 flex flex-col gap-2">
-      <Collapse />
-      <Collapse />
-      <Collapse />
+      {questions.map((question) => (
+        <Collapse key={question._id} questionNumber={question.number} questionName={question.title} questionDifficulty={question.difficulty} />
+      ))}
     </div>
   );
 };
