22f56fdbdd2cd2198962fdb24f8d5e9c16f89c97
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-01 11:05:14 -0500

    Adding react-icons package and adding delete and edit buttons to the question cards.

--- client/leetcode-tracker-frontend/src/components/QuestionCard.tsx
+++ client/leetcode-tracker-frontend/src/components/QuestionCard.tsx
@@ -1,3 +1,5 @@
+import { FaTrash, FaEdit } from "react-icons/fa";
+
 type QuestionCardProps = {
   title: string;
   questionNumber: number;
@@ -13,6 +15,14 @@ const QuestionCard = ({ title, questionNumber, difficulty }: QuestionCardProps) 
           <li>Question Number: {questionNumber}</li>
           <li>Difficulty: {difficulty}</li>
         </ul>
+        <div className="flex justify-end gap-2 text-xl">
+          <button>
+            <FaTrash />
+          </button>
+          <button>
+            <FaEdit />
+          </button>
+        </div>
       </div>
     </div>
   );
